41eee8bfe82d45fdbb41ff2c119c8abe8bee6c2b
Orlando J Betancourth <orlando.betancourth@gmail.com>
2020-02-03 19:05:53 -0600

    Desarrollo de POST y PUT del API y pruebas en POSTMAN

--- bckLencaStore/routes/api/seguridad/seguridad.js
+++ bckLencaStore/routes/api/seguridad/seguridad.js
@@ -30,6 +30,17 @@ router.get('/users/:id',(req, res)=>{
     return res.status(200).json(user);
 });
 
+// http://localhost:3000/api/seguridad/users/new
+router.post('/users/new', (req, res)=>{
+  var datosEnviados = req.body;
+  var newUser = userModel.addNew(datosEnviados);
+  return res.status(200).json(newUser);
+}); // post users/new
 
+router.put('/users/upd/:id', (req, res)=>{
+  var id = parseInt(req.params.id);
+  var updUser = userModel.update( id, req.body);
+  return res.status(200).json(updUser);
+});
 
 module.exports = router;
--- bckLencaStore/routes/api/seguridad/seguridad.model.js
+++ bckLencaStore/routes/api/seguridad/seguridad.model.js
@@ -27,6 +27,53 @@ userModel.getById = (id)=>{
   }
 }
 
+userModel.addNew = ({ useremail, userpswd, usernames }  )=>{
+  var newUser = Object.assign(
+    {},
+    userTemplate,
+    {
+      userEmail: useremail,
+      userPswd: userpswd,
+      userCompleteName: usernames,
+      userDateCreated: new Date().getTime()
+    }
+  );
+  newUser.userID = userCollection.length + 1;
+
+  userCollection.push(newUser);
+  return newUser;
+}
+
+userModel.update = (id, { userpswd, usernames })=>{
+ var updatingUser = userCollection.filter(
+   (o, i)=>{
+     return o.userID === id;
+   }
+ );
+ if(updatingUser && updatingUser.length>0){
+   updatingUser = updatingUser[0];
+ } else {
+   return null;
+ }
+ var updateUser = {};
+ var newUpdatedCollection = userCollection.map(
+   (o, i)=>{
+     if(o.userID === id){
+       updateUser = Object.assign({},
+          o,
+         { userPswd: userpswd, userCompleteName:usernames}
+       );
+       return updateUser;
+     }else{
+       return o;
+     }
+   }
+ );
+  userCollection = newUpdatedCollection;
+  return updateUser;
+}
+
+
 userCollection.push(
   Object.assign(
     {},
