a1a883ebe3367984f52fd56532e084d7ae0a5653
Orlando J Betancourth <orlando.betancourth@gmail.com>
2020-03-30 18:52:47 -0600

    Infinite Scroller de Productos Artesanos

--- frnlencastore/src/componentes/Pages/Private/Products/ProductList.js
+++ frnlencastore/src/componentes/Pages/Private/Products/ProductList.js
@@ -1,38 +1,87 @@
-import React, {Component} from 'react';
-import {Link, Redirect} from 'react-router-dom';
-import {saxios} from '../../../Utilities/Utilities';
+import  React, { Component } from 'react';
+import InfiniteScroll from 'react-infinite-scroller';
+import { IoIosInformationCircleOutline, IoIosSync, IoMdAddCircle } from 'react-icons/io';
+import {Link} from 'react-router-dom';
+
 import Page from '../../Page';
-
-export default class ProductList extends Component{
-  constructor(){
+import { saxios } from '../../../Utilities/Utilities.js';
+import './List.css';
+export default class Products extends Component {
+  constructor(){;
     super();
-    this.state = {
-      products:[],
-      page:0,
-      loading:false,
+    this.state={
+     items:[],
+     hasMore:true,
+     page:1,
+     itemsToLoad:10
     }
+    this.loadMore = this.loadMore.bind(this);
+ }
+ loadMore(page){
+   const items  = this.state.itemsToLoad;
+   const uri = `/api/artesanos/products/${page}/${items}`;
+   saxios.get(uri)
+     .then(
+       ({data})=>{
+         console.log(data);
+         const { products: apiItems, total} = data;
+         const loadedItems = this.state.items;
+         apiItems.map((e)=>loadedItems.push(e));
+         if(total){
+             this.setState({
+               'items': loadedItems,
+               'hasMore': (page * items < total)
+              });
+          } else {
+            this.setState({
+              'hasMore': false
+            });
+          }
+        }
+      )
+      .catch(
+        (err)=>{
+          console.log(err);
+        }
+      );
   }
-  componentDidMount(){
-    //cargar los productos
-    saxios.get(
-      "/api/version"
-    )
-    .then((resp)=>{
-      console.log(resp.data);
-    })
-    .catch((error)=>{
-      console.log(error);
-    });
+  render() {
+  const uiItems = this.state.items.map(
+    (item)=>{
+      return (
+        <div className='listItem' key={item._id}>
+          <span>{item.sku} {item.name}</span>
+          <span className='updateListItem'>
+            <Link to={`/detailChangeThis/${item._id}`}>
+              <IoIosInformationCircleOutline size='2em'/>
+            </Link>
+          </span>
+        </div>);
+    }
+  );
 
-  }
-  render(){
-    return (
-      <Page pageTitle="Productos" auth={this.props.auth}>
-          <ul>
-          <li>Productos</li>
-          </ul>
-      </Page>
-    );
-  }
+  if (!uiItems.length) uiItems.push(
+    <div className="listItem" key="pbListAddOne">
+      <span>Nuevo Item</span>
+      <Link to="/changeThis"><IoMdAddCircle size="2.5em" /></Link>
+    </div>);
 
+  return (
+    <Page title="My List Change This" auth={this.props.auth}>
+      <div className="list" ref={(ref)=> this.scrollParentRef = ref}>
+          <InfiniteScroll
+            pageStart={0}
+            loadMore={this.loadMore}
+            hasMore={this.state.hasMore}
+            useWindow={false}
+            threshold={108}
+            getScrollParent={()=>this.scrollParentRef}
+            loader={<div key="pbListLoading" className="listItem center"><IoIosSync/></div>}
+            >
+              {uiItems}
+          </InfiniteScroll>
+      </div>
+     </Page>
+   );
+  }
 }
